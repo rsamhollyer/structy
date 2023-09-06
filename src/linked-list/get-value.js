73f18857002a32800434ec6822ab0db4cc759840
Sam Hollyer <rsamhollyer@gmail.com>
2023-09-06 15:13:24 -0500

    update

---
+++ src/linked-list/get-value.js
@@ -0,0 +1,87 @@
+import Node from './helpers/node-class.js';
+
+/**
+ *
+ * @param {Node} head
+ * @param {number} index
+ *
+ * @returns {any} value
+ */
+function getNodeValue(head, index) {
+  if (index < 0 || head === null) return null;
+
+  let current = head;
+  let count = 0;
+  let value = null;
+
+  while (count <= index && current !== null) {
+    if (count === index) {
+      value = current.val;
+      break;
+    } else {
+      current = current.next;
+      count++;
+    }
+  }
+  console.log(value);
+  return value;
+}
+
+/* ********************************* */
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+getNodeValue(a, 22); // 'c'
+
+const a2 = new Node('a');
+const b2 = new Node('b');
+const c2 = new Node('c');
+const d2 = new Node('d');
+
+a2.next = b2;
+b2.next = c2;
+c2.next = d2;
+
+// a -> b -> c -> d
+
+getNodeValue(a2, 3); // 'd'
+
+const a3 = new Node('a');
+const b3 = new Node('b');
+const c3 = new Node('c');
+const d3 = new Node('d');
+
+a3.next = b3;
+b3.next = c3;
+c3.next = d3;
+
+// a -> b -> c -> d
+
+getNodeValue(a3, 7); // null
+
+const node1 = new Node('banana');
+const node2 = new Node('mango');
+
+node1.next = node2;
+
+// banana -> mango
+
+getNodeValue(node1, 0); // 'banana'
+
+const node3 = new Node('banana');
+const node4 = new Node('mango');
+
+node3.next = node4;
+
+// banana -> mango
+
+getNodeValue(node3, 1); // 'mango'
